a1a0a7632c83e4d824cbfe6ddc7b0ef1d0ea9a2e
Simon Hawe <simon.hawe@gmail.com>
2019-12-05 20:50:10 +0100

    Add eda.py

--- awesome_pandas/apply_example.py
+++ awesome_pandas/apply_example.py
@@ -1,9 +1,20 @@
 import pandas as pd
 import numpy as np
 from tqdm import tqdm
+
 # Register the progress_apply method
 tqdm.pandas(desc="Hello Medium")
 
+
+def dummy_func(s):
+    return np.abs(s) ** 1 / 2
+
+
 # Create some random but "large" dataset wiht 10 000 rows.
-df = pd.DataFrame(np.random.rand(10000,10))
-result = df.progress_apply(lambda x:np.abs(x)**1/2, axis=1)
+df = pd.DataFrame(np.random.rand(10000, 10))
+result_basic = df.progress_apply(dummy_func, axis=1)
+
+import swifter
+
+result_swifter = df.swifter.apply(dummy_func, axis=1)
+result_swifter = df.swifter.progress_bar(True).apply(dummy_func, axis=1)
--- awesome_pandas/eda.py
+++ awesome_pandas/eda.py
@@ -1,42 +1,16 @@
-import numpy as np
 import pandas as pd
-import awesome_pandas.report_extension
+import pandas_profiling
 import webbrowser
 import seaborn as sns
 
 
-def gen_data(n_samples: int) -> pd.DataFrame:
-    data = {}
-    uncorrelated = np.random.standard_normal((3, n_samples))
-    data["normal-1"] = uncorrelated[0, :] + 123.12
-    for i, cor in enumerate((0.2, 0.95)):
-        L = np.linalg.cholesky([[1, cor], [cor, 1]])
-        data[f"normal-1-{i}"] = (
-            np.dot(L, uncorrelated[:2, :]) + np.random.rand(2, 1) * 100
-        )[1, :]
-        data[f"normal-2-{i}"] = (
-            np.dot(L, uncorrelated[[0, 2], :]) + np.random.rand(2, 1) * 100
-        )[1, :]
-
-    data["log-normal"] = np.random.lognormal(19.45, 4.23, n_samples)
-    data["exp"] = np.random.exponential(14.54, n_samples)
-    data["exp"][
-        np.random.choice(n_samples, int(n_samples * 0.15), replace=False)
-    ] = np.nan
-
-    data["rand-ints-1"] = np.random.randint(0, 8, n_samples)
-    data["rand-ints-2"] = np.random.randint(4, 100, n_samples)
-    data["probs"] = np.random.random(n_samples)
-    data["probs"][
-        np.random.choice(n_samples, int(n_samples * 0.05), replace=False)
-    ] = np.nan
-
-    return pd.DataFrame(data)
-
-
-if False:
-    df = sns.load_dataset("titanic")
-    report = df.profile_report(style={"full_width": True})
-    html_file = "output.html"
+def open_report(df: pd.DataFrame):
+    report = df.profile_report(style={'full_width': True})
+    html_file = "probably-some-unique-file-name.html"
     report.to_file(output_file=html_file)
     webbrowser.open(html_file, new=2)
+
+
+if __name__ == '__main__':
+    df = sns.load_dataset("titanic")
+    open_report(df)
